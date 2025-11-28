e6f4381ad70f12904150f50d965ea40efa5409dd
Xyphes <willy.somkhit@epita.fr>
2025-11-28 09:37:42 +0100

    feat: Project init

---
+++ src/main/java/org/formation/projet_somkhit_willy/ProjetSomkhitWillyApplication.java
@@ -0,0 +1,13 @@
+package org.formation.projet_somkhit_willy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSomkhitWillyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSomkhitWillyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/formation/projet_somkhit_willy/ProjetSomkhitWillyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.projet_somkhit_willy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSomkhitWillyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
